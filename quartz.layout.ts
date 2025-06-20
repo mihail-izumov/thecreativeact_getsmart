960239f8e81f93ab0e55d8fedd3ecec1f409265d
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-06-20 17:13:56 +0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -2,25 +2,18 @@ import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
 // components shared across all pages
+// components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.Links({
-      links: {
-        "Главная": "/",
-        "Блог": "/blog",
-        "Документация": "/docs",
-      },
-    }),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
       "Кто первый": { link: "https://thecreativeact.ru", target: "_blank" },
       "Телеграм-канал": "https://t.me/runscale",
-      "Информация о сайте": "/credits",
-      "Контакт": "mailto:izumov@thecreativeact.ru",
-      "О Модуле Роста": "https://kto1.io"
+  "Информация о сайте": "/credits",
+  "Контакт": "mailto:izumov@thecreativeact.ru",
+  "О Модуле Роста": "https://kto1.io"
     },
   }),
 }
@@ -39,13 +32,17 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    //Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
+    //Component.Graph(),
     Component.DesktopOnly(Component.Explorer()),
+    //Component.DesktopOnly(Component.TableOfContents()),
+    //Component.Backlinks(),
   ],
 }
 
-// components for pages that display lists of pages (e.g. tags or folders)
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
