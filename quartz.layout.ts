589a5b41d29f3961696fb3e316e27820c9898d53
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-06-20 16:59:38 +0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,6 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
+      "Кто первый": { link: "https://thecreativeact.ru", target: "_blank" },
       "Телеграм-канал": "https://t.me/runscale",
   "Информация о сайте": "/credits",
   "Контакт": "mailto:izumov@thecreativeact.ru",
