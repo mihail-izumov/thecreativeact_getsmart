fa20449faf4028d9e5d0d853a83060b90730c6f6
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-06-20 17:04:14 +0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Блог Модуля Роста®",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
